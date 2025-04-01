b06700a44bf0eb1ccef922898ae88bd5ba22a1ba
PabloJGV <caveroescudero.pablo@jgvaldemora.org>
2025-04-01 09:37:03 +0200

    Actualización de archivo java

--- iniciales/src/App.java
+++ iniciales/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Mis iniciales son P y C");
+        System.out.println("Esta es la actualización");
     }
 }
